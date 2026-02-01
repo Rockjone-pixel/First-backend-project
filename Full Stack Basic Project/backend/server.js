3999957f144aa1abd06a15d9d977590ab89cd060
Rockjone-pixel <ayushmansagar46@gmail.com>
2026-02-02 01:07:24 +0530

    Commit it

--- Full Stack Basic Project/backend/server.js
+++ Full Stack Basic Project/backend/server.js
@@ -1,35 +1,38 @@
-import express from 'express';
+import express from "express";
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Server is ready , Ayushman!');
+app.get("/", (req, res) => {
+  res.send("READY!");
 });
 
 // get a list of 5 jokes
 
-app.get('/jokes', (req, res) => {
+app.get("/jokes", (req, res) => {
   const jokes = [
     {
-        id : 1,
-        title : 'A joke',
-        content : "Why don't scientists trust atoms? Because they make up everything!"
+      id: 1,
+      title: "A joke",
+      content:
+        "Why don't scientists trust atoms? Because they make up everything!",
     },
     {
-        id : 2,
-        title : 'B joke',
-        content : "Why did the scarecrow win an award? Because he was outstanding in his field!"
+      id: 2,
+      title: "B joke",
+      content:
+        "Why did the scarecrow win an award? Because he was outstanding in his field!",
     },
     {
-        id : 3,
-        title : 'C joke',
-        content : "Why don't skeletons fight each other? They don't have the guts!"
-    }
-  ]
-    res.json(jokes);
-  });
+      id: 3,
+      title: "C joke",
+      content:
+        "Why don't skeletons fight each other? They don't have the guts!",
+    },
+  ];
+  res.json(jokes);
+});
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running SUCCESSFULLY on http://localhost:${port}`);
 });
